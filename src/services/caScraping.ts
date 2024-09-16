77129e6ac7716777ec61f82455e0d74fdf183f6c
BeGoD <55290097+tfhgzeo@users.noreply.github.com>
2024-09-16 04:51:21 -0300

    update
    atualizada as dependencias e o caScraping para utilizar o playwright

--- src/services/caScraping.ts
+++ src/services/caScraping.ts
@@ -1,47 +1,30 @@
-import {By, Builder, Browser} from 'selenium-webdriver'
-const firefox = require('selenium-webdriver/firefox');
+const playwright = require('playwright');
 
-export const caScraping = async (ca: string): Promise<caScrapingInfos> => {
+export const playwrightCaScraping = async (ca: string): Promise<caScrapingInfos> => {
   return new Promise(async (resolve, reject) => {
     let infos: caScrapingInfos 
-
-    let options = new firefox.Options();
-    options.addArguments('--headless'); // Ativar o modo headless
-    
-  
-    let driver = await new Builder()
-      .forBrowser(Browser.FIREFOX)
-      .setFirefoxOptions(options)
-      .build();
-
-    try {    
-      await driver.get(`https://consultaca.com/${ca}`)
-  
-      const descricao = await driver.findElement(By.xpath("//div[h3[text()='Descrição Completa']]/p[@class='info']")).getText()
-      const nome = await driver.findElement(By.xpath("/html/body/form/div[3]/div[4]/div[1]/div/div[1]/div/h1")).getText()
-      const nomeComercial = await driver.findElement(By.xpath("//div[h3[text()='Nome Comercial (Modelo)']]/p[@class='info']")).getText()
-      const validade = await driver.findElement(By.xpath("//div[@id='box_result']/p[strong[text()='Validade:']]/span[@class='validade_ca regular']")).getText()
-      const epiImage = await driver.findElement(By.xpath("//div[h3[text()='Fotos do Equipamento']]/ul/li/a/img")).getAttribute("src")
-      const razaoSocialImportador = await driver.findElement(By.xpath("//div[h3[text()='Fabricante']]/div/p[strong[text()='Razão Social Importador:']]")).getText()
-      const cnpjImportador = await driver.findElement(By.xpath("//div[h3[text()='Fabricante']]/div/p[strong[text()='CNPJ Importador:']]")).getText()
-      const cidadeUF = await driver.findElement(By.xpath("//div[h3[text()='Fabricante']]/div/p[strong[text()='Cidade/UF:']]")).getText()
-      const normas = await driver.findElement(By.xpath("//div[h3[text()='Normas']]/ul")).getText()
-      const numLaudo = await driver.findElement(By.xpath("//div[h3[text()='Laudos']]/p[strong[text()='N° do Laudo:']]")).getText()
-      const cnpjLaboratorio = await driver.findElement(By.xpath("//div[h3[text()='Laudos']]/p[strong[text()='CNPJ do Laboratório:']]")).getText()
-      const razaoSocialLaboratorio = await driver.findElement(By.xpath("//div[h3[text()='Laudos']]/p[strong[text()='Razão Social:']]")).getText()
-  
-      // console.log('Nome: ' + nome);
-      // console.log('Validade: ' + validade);
-      // console.log('Nome Comercial: ' + nomeComercial)
-      // console.log('Descrição: ' + descricao)
-      // console.log('Imagem EPI: ', epiImage)
-      // console.log('Razão Social do Importador: ', razaoSocialImportador.toString().split("\n")[1])
-      // console.log('CNPJ Importador: ' + cnpjImportador.toString().split("\n")[1])
-      // console.log('Cidade/UF: ' + cidadeUF.toString().split("\n")[1])
-      // console.log("Normas: " + normas);
-      // console.log("Número do Laudo: " + numLaudo.toString().split("\n")[1]);
-      // console.log("CNPJ do Laboratório: " + cnpjLaboratorio.toString().split("\n")[1]);
-      // console.log("Razão Social do Laboratório: " + razaoSocialLaboratorio.toString().split("\n")[1]);
+    const browser = await playwright['chromium'].launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    try {
+      await page.goto(`https://consultaca.com/${ca}`);
+      const descricao = await page.locator("xpath=//div[h3[text()='Descrição Completa']]/p[@class='info']").textContent();
+      const nome = await page.locator("xpath=/html/body/form/div[3]/div[4]/div[1]/div/div[1]/div/h1").textContent();
+      const nomeComercial = await page.locator("xpath=//div[h3[text()='Nome Comercial (Modelo)']]/p[@class='info']").textContent();
+      const validade = await page.locator("xpath=//div[@id='box_result']/p[strong[text()='Validade:']]/span[@class='validade_ca regular']").textContent();
+      const epiImages = await page.locator("xpath=//div[h3[text()='Fotos do Equipamento']]/ul/li/a/img").all();
+      let epiImage
+      for (const img of epiImages) {
+        const src = await img.getAttribute("src");
+        epiImage = src
+      }
+      const razaoSocialImportador = await page.locator("xpath=//div[h3[text()='Fabricante']]/div/p[strong[text()='Razão Social Importador:']]").textContent() 
+      const cnpjImportador = await page.locator("xpath=//div[h3[text()='Fabricante']]/div/p[strong[text()='CNPJ Importador:']]").textContent()
+      const cidadeUF = await page.locator("xpath=//div[h3[text()='Fabricante']]/div/p[strong[text()='Cidade/UF:']]").textContent()
+      const normas = await page.locator("xpath=//div[h3[text()='Normas']]/ul/li").allTextContents()
+      const numLaudo = await page.locator("xpath=//div[h3[text()='Laudos']]/p[strong[text()='N° do Laudo:']]").textContent();
+      const cnpjLaboratorio = await page.locator("xpath=//div[h3[text()='Laudos']]/p[strong[text()='CNPJ do Laboratório:']]").textContent();
+      const razaoSocialLaboratorio = await page.locator("xpath=//div[h3[text()='Laudos']]/p[strong[text()='Razão Social:']]").textContent();
 
       infos = {
         nome,
@@ -50,20 +33,20 @@ export const caScraping = async (ca: string): Promise<caScrapingInfos> => {
         descricao,
         imagem: epiImage.toString(),
         importador: {
-          razaoSocial: razaoSocialImportador,
-          cnpj: cnpjImportador,
-          cidadeUf: cidadeUF
+          razaoSocial: razaoSocialImportador.split(':')[1],
+          cnpj: cnpjImportador.split(':')[1],
+          cidadeUf: cidadeUF.split(':')[1]
         },
-        normas: normas.split('\n'),
+        normas: normas,
         laudo: {
-          numero: numLaudo,
-          cnpj: cnpjLaboratorio,
-          razaoSocial: razaoSocialLaboratorio
+          numero: numLaudo.split(':')[1],
+          cnpj: cnpjLaboratorio.split(':')[1],
+          razaoSocial: razaoSocialLaboratorio.split(':')[1]
         }
       }
     }
     finally {
-      await driver.quit();
+      await browser.close();
       resolve(infos);
     }
   })
