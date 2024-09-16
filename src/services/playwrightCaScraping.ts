f80cae7a32bf23390b51366c2b7f81c639ca6dae
BeGoD <55290097+tfhgzeo@users.noreply.github.com>
2024-09-16 04:49:06 -0300

    Update playwrightCaScraping.ts

--- src/services/playwrightCaScraping.ts
+++ src/services/playwrightCaScraping.ts
@@ -7,17 +7,21 @@ export const playwrightCaScraping = async (ca: string): Promise<caScrapingInfos>
     const context = await browser.newContext();
     const page = await context.newPage();
     try {
-
       await page.goto(`https://consultaca.com/${ca}`);
       const descricao = await page.locator("xpath=//div[h3[text()='Descrição Completa']]/p[@class='info']").textContent();
       const nome = await page.locator("xpath=/html/body/form/div[3]/div[4]/div[1]/div/div[1]/div/h1").textContent();
       const nomeComercial = await page.locator("xpath=//div[h3[text()='Nome Comercial (Modelo)']]/p[@class='info']").textContent();
       const validade = await page.locator("xpath=//div[@id='box_result']/p[strong[text()='Validade:']]/span[@class='validade_ca regular']").textContent();
-      const epiImage = await page.locator("xpath=//div[h3[text()='Fotos do Equipamento']]/ul/li/a/img").getAttribute("src");
-      const razaoSocialImportador = await page.locator("xpath=//div[h3[text()='Fabricante']]/div/p[strong[text()='Razão Social Importador:']]").textContent();
-      const cnpjImportador = await page.locator("xpath=//div[h3[text()='Fabricante']]/div/p[strong[text()='CNPJ Importador:']]").textContent();
-      const cidadeUF = await page.locator("xpath=//div[h3[text()='Fabricante']]/div/p[strong[text()='Cidade/UF:']]").textContent();
-      const normas = await page.locator("xpath=//div[h3[text()='Normas']]/ul").textContent();
+      const epiImages = await page.locator("xpath=//div[h3[text()='Fotos do Equipamento']]/ul/li/a/img").all();
+      let epiImage
+      for (const img of epiImages) {
+        const src = await img.getAttribute("src");
+        epiImage = src
+      }
+      const razaoSocialImportador = await page.locator("xpath=//div[h3[text()='Fabricante']]/div/p[strong[text()='Razão Social Importador:']]").textContent() 
+      const cnpjImportador = await page.locator("xpath=//div[h3[text()='Fabricante']]/div/p[strong[text()='CNPJ Importador:']]").textContent()
+      const cidadeUF = await page.locator("xpath=//div[h3[text()='Fabricante']]/div/p[strong[text()='Cidade/UF:']]").textContent()
+      const normas = await page.locator("xpath=//div[h3[text()='Normas']]/ul/li").allTextContents()
       const numLaudo = await page.locator("xpath=//div[h3[text()='Laudos']]/p[strong[text()='N° do Laudo:']]").textContent();
       const cnpjLaboratorio = await page.locator("xpath=//div[h3[text()='Laudos']]/p[strong[text()='CNPJ do Laboratório:']]").textContent();
       const razaoSocialLaboratorio = await page.locator("xpath=//div[h3[text()='Laudos']]/p[strong[text()='Razão Social:']]").textContent();
@@ -29,15 +33,15 @@ export const playwrightCaScraping = async (ca: string): Promise<caScrapingInfos>
         descricao,
         imagem: epiImage.toString(),
         importador: {
-          razaoSocial: razaoSocialImportador,
-          cnpj: cnpjImportador,
-          cidadeUf: cidadeUF
+          razaoSocial: razaoSocialImportador.split(':')[1],
+          cnpj: cnpjImportador.split(':')[1],
+          cidadeUf: cidadeUF.split(':')[1]
         },
-        normas: normas.split('\n'),
+        normas: normas,
         laudo: {
-          numero: numLaudo,
-          cnpj: cnpjLaboratorio,
-          razaoSocial: razaoSocialLaboratorio
+          numero: numLaudo.split(':')[1],
+          cnpj: cnpjLaboratorio.split(':')[1],
+          razaoSocial: razaoSocialLaboratorio.split(':')[1]
         }
       }
     }
